c7bd4d120146d41bcf61577800a50733d1d49723
Ludo Loops <ludoloops@prontonmail.com>
2020-12-05 23:25:30 +0100

    feat : python env

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/loops/data/A-dev_projects/Projet/bell-project/django/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
